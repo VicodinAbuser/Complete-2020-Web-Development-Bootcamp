c4349fd63383bed7aea2a05af8688b215dcd85ef
Naman Agarwal <thenamanagarwal@gmail.com>
2020-07-11 03:23:19 +0530

    Added sounds for the correct keyboard key presses

--- Drum Kit/Drum Kit Starting Files/index.js
+++ Drum Kit/Drum Kit Starting Files/index.js
@@ -7,45 +7,59 @@ for (let i = 0; i < drums.length; i++) {
 
         let buttonText = this.innerHTML;
 
-        switch(buttonText) {
-            case "w":
-                let sound1 = new Audio('./sounds/crash.mp3');
-                sound1.play();
-                break;
+        whichKey(buttonText);
 
-            case "a":
-                let sound2 = new Audio('./sounds/kick-bass.mp3');
-                sound2.play();
-                break;
-
-            case "s":
-                let sound3 = new Audio('./sounds/snare.mp3');
-                sound3.play();
-                break;
-
-            case "d":
-                let sound4 = new Audio('./sounds/tom-1.mp3');
-                sound4.play();
-                break;
-
-            case "j":
-                let sound5 = new Audio('./sounds/tom-2.mp3');
-                sound5.play();
-                break;
-
-            case "k":
-                let sound6 = new Audio('./sounds/tom-3.mp3');
-                sound6.play();
-                break;
-
-            case "l":
-                let sound7 = new Audio('./sounds/tom-4.mp3');
-                sound7.play();
-                break;
-
-            default:
-                console.log(innerHTML);
-        }
      });
 
+}
+
+
+document.addEventListener('keydown', function(event) {
+
+    let buttonText = event.key;
+    whichKey(buttonText);
+
+    })
+
+
+function whichKey(character) {
+    switch(character) {
+        case "w":
+            let sound1 = new Audio('./sounds/crash.mp3');
+            sound1.play();
+            break;
+
+        case "a":
+            let sound2 = new Audio('./sounds/kick-bass.mp3');
+            sound2.play();
+            break;
+
+        case "s":
+            let sound3 = new Audio('./sounds/snare.mp3');
+            sound3.play();
+            break;
+
+        case "d":
+            let sound4 = new Audio('./sounds/tom-1.mp3');
+            sound4.play();
+            break;
+
+        case "j":
+            let sound5 = new Audio('./sounds/tom-2.mp3');
+            sound5.play();
+            break;
+
+        case "k":
+            let sound6 = new Audio('./sounds/tom-3.mp3');
+            sound6.play();
+            break;
+
+        case "l":
+            let sound7 = new Audio('./sounds/tom-4.mp3');
+            sound7.play();
+            break;
+
+        default:
+            console.log(innerHTML);
+    }
 }
