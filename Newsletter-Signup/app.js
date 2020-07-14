f6966edb742726b15df1f5bf906092939bc1ac2a
Naman Agarwal <thenamanagarwal@gmail.com>
2020-07-14 23:47:11 +0530

    Added succes and failure pages and a whole lot more

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
+const https = require('https');
 
 const app = express();
 
@@ -12,16 +13,59 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    let firstName = req.body.firstName;
-    let secondName = req.body.secondName;
-    let email = req.body.email;
+    const firstName = req.body.firstName;
+    const secondName = req.body.secondName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: secondName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = 'https://us10.api.mailchimp.com/3.0/lists/f7106fc297';
+    const options = {
+        method: "POST",
+        auth: "naman:abffb77b889b85801785cbc50ce67fa31-us10"
+    };
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
     console.log(firstName, secondName, email);
 });
 
 
-
-
-
+app.post('/failure', (req, res) => {
+    res.redirect('/');
+})
 
 
 app.listen('3000', () => { console.log("Server is running on port 3000") });
+
+// API Key
+// bffb77b889b85801785cbc50ce67fa31-us10
+
+// List ID
+// f7106fc297
