451bb88252ffa873f03f8b5b58cbf085585209c2
Naman Agarwal <thenamanagarwal@gmail.com>
2020-07-19 00:30:24 +0530

    Add database to blog website

--- EJS Challenge Starting Files/ejs-challenge/app.js
+++ EJS Challenge Starting Files/ejs-challenge/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
+// const posts = [];
 
 const app = express();
 
@@ -16,8 +17,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    post: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+
+
+
+
 app.get('/', (req, res) => {
-    res.render('home', {content: homeStartingContent, postsArray: posts});
+    Post.find((err, posts) => {
+        console.log(posts);
+        res.render('home', {content: homeStartingContent, postsArray: posts});
+
+    })
 });
 
 app.get('/about', (req, res) => {
@@ -33,22 +52,27 @@ app.get('/compose', (req, res) => {
 });
 
 app.post('/compose', (req, res) => {
-    const post = {
-        postTitle: req.body.title,
-        postContent: req.body.post
-    };
-    posts.push(post);
-    res.redirect('/');
+    const post = new Post ({
+        title: req.body.title,
+        post: req.body.post
+    });
+
+    post.save((err) => {
+        if(!err) {
+            res.redirect('/');
+        }
+    });
 })
 
-app.get('/posts/:post', (req, res) => {
-    let requestedTitle = lodash.lowerCase(req.params.post);
-    for (let i = 0; i < posts.length; i++) {
-        if (lodash.lowerCase(posts[i].postTitle) === requestedTitle) {
-            res.render('post', {title: posts[i].postTitle, content: posts[i].postContent});
-            break;
-        }
-    }
+app.get('/posts/:postId', (req, res) => {
+
+    const requestedPostId = req.params.postId;
+
+    Post.findOne({_id: requestedPostId}, (err, foundPost) => {
+
+        res.render('post', {title: foundPost.title, content: foundPost.post});
+
+    });
 
 });
 
