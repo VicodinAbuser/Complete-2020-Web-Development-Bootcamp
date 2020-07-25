25997321670e718fb5edd573b91ba6167263d180
Naman Agarwal <thenamanagarwal@gmail.com>
2020-07-25 17:28:24 +0530

    A simple contacts page made using React

---
+++ react-contact-page/src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./Card.jsx";
+import contacts from "../contacts.js";
+
+console.log(contacts);
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        mail={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        mail={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        mail={contacts[2].email}
+      />
+    </div>
+  );
+}
+
+export default App;
