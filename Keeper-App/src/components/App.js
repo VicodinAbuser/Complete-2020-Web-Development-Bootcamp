4c1eba60575833ddb3f7a03561c19b708bd73666
Naman Agarwal <thenamanagarwal@gmail.com>
2020-07-26 17:08:00 +0530

    Keepr App - Initial Commit

---
+++ Keeper-App/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes.js";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(note => {
+        return (
+          <Note key={note.key} title={note.title} content={note.content} />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
