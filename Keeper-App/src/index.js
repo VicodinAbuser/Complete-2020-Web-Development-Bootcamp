b162b7432db4396e193a60f1bdc108ddc9eee838
Naman Agarwal <thenamanagarwal@gmail.com>
2020-07-27 17:25:23 +0530

    Add delete functionality

--- Keeper-App/src/index.js
+++ Keeper-App/src/index.js
@@ -3,6 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
